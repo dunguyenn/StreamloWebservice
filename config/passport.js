def07c95478c8d4e6ac392ce2761ad4f0b8ecbc1
Richard Taylor <richard534@hotmail.co.uk>
2016-04-22 17:48:28 +0100

    Removed unnessesary printing to console

--- config/passport.js
+++ config/passport.js
@@ -5,15 +5,11 @@ module.exports = function (app) {
     app.use(passport.session());
 
     passport.serializeUser(function(user, done){
-        console.log('seralize');
-        console.log('user:' + user);
         //push entire object to session
         done(null, user); // replace with user.id
     });
 
     passport.deserializeUser(function (user, done) { // Replace user with userId
-        console.log('de seralize');
-        console.log(user);
         //mongo.findbyId
         done(null, user);
     });
